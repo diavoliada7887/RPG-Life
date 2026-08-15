35d6c287a57adcbdc55356609d30af731bcf1929
diavoliada7887 <diavoliada666@gmail.com>
2026-08-15 18:38:43 +0500

    Refresh cache for creative lines and calorie bank

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='rpg-life-v13-practice-templates-daily-logs';
-const ASSETS=['./','./index.html','./styles.css','./fantasy.css','./rpg-v2.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='rpg-life-v14-creative-calorie-bank';
+const ASSETS=['./','./index.html','./styles.css','./fantasy.css','./rpg-v2.css','./rpg-v4.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
