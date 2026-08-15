818dd55ecf57490bbde580237c7649434bb564b7
diavoliada7887 <diavoliada666@gmail.com>
2026-08-15 18:54:31 +0500

    Refresh cache for simplified daily bank

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='rpg-life-v15-calorie-bank-remaining';
-const ASSETS=['./','./index.html','./styles.css','./fantasy.css','./rpg-v2.css','./rpg-v4.css','./app.js','./calorie-bank.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='rpg-life-v16-simple-daily-bank';
+const ASSETS=['./','./index.html','./styles.css','./fantasy.css','./rpg-v2.css','./rpg-v4.css','./app.js','./calorie-bank.js','./rpg-simple-bank.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
